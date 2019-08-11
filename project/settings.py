977ad1809ca4eadd38ccecac5b020e6a4b2d5dc2
John Tran <johnkntran@users.noreply.github.com>
2019-08-10 22:34:45 -0400

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = './static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Configure Django LiveReload server
